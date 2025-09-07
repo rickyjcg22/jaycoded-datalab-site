19141e6d63ca2c934a3126d155a07fea16800ea0
rickyjcg22 <rjcgomez@mymail.mapua.edu.ph>
2025-09-07 18:24:36 +0800

    Adding Announcement section

---
+++ src/Components/Announcement/Announcement.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Announcement.css'
+
+const Announcement = () => {
+    return(
+        <div className="announcement">
+            Site in progress. New updates are coming!
+        </div>
+        
+    );
+};
+
+export default Announcement;
